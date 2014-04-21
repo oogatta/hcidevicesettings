2dd8dabb08d754293c8be27df601996eba8b9098
oogatta <oogatta@gmail.com>
2014-04-21 10:40:56 +0900

    internal only Dvorak

--- osx/keyremap4macbook.sh
+++ osx/keyremap4macbook.sh
@@ -2,18 +2,14 @@
 
 cli=/Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli
 
-$cli set general.dont_remap_internal 1
-/bin/echo -n .
-$cli set remap.jis_optionL2optionL_eisuu 1
-/bin/echo -n .
-$cli set repeat.wait 33
+$cli set repeat.wait 32
 /bin/echo -n .
 $cli set repeat.initial_wait 300
 /bin/echo -n .
-$cli set remap.copy_paste_winstyle3_TypeMatrixOnly 1
+$cli set private.oogatta_oneshot 1
 /bin/echo -n .
 $cli set private.oogatta_anker 1
 /bin/echo -n .
-$cli set remap.jis_optionR2optionR_kana 1
+$cli set private.oogatta_internal_dvorak 1
 /bin/echo -n .
 /bin/echo
